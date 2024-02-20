14b28b73def15a83cb7cd99d6f997f042d80a6de
silentFellow <thegreatestintelligent+github@gmail.com>
2024-02-20 22:35:03 +0530

    login button place changed

--- iitm-streamer-web/src/App.jsx
+++ iitm-streamer-web/src/App.jsx
@@ -6,7 +6,6 @@ import Devicedetails from "./Pages/Devicedetails";
 import ListCases from "./Pages/ListCases";
 import CaseDetails from "./Pages/CaseDetails";
 import Logo from "./assets/Tamil_Nadu_Police_Logo.png";
-import Login from "./Components/Login";
 
 // #03045e
 // #0077b6
@@ -24,7 +23,17 @@ function App() {
 
         <div className="navbar bg-[#03045e] flex justify-between">
           <img className="h-[50px] w-[50px]" src={Logo} alt="" />
-          <button className="p-2 px-3 bg-black text-white" onClick={() => setLogin(true)}>Login</button>
+          <div className="flex gap-3">
+            { localStorage.getItem("name") !== null && <h2 className="text-white">Welcome {localStorage.getItem("name")}</h2> }
+            <button
+              className="p-2 px-3 bg-black text-white"
+              onClick={() => {
+                localStorage.getItem("name") === null ? setLogin(true) : (localStorage.clear(), window.location.reload())
+              }}
+            >
+              { localStorage.getItem("name") === null ? "Login" : "Sign out" }
+            </button>
+          </div>
         </div>
         <div className="flex">
           <SideBar />
